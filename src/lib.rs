944ab1e441ea3ac9037540f7ec6dc70e0a24e0ac
Nathan Sizemore <nathanrsizemore@gmail.com>
2015-06-17 00:41:43 -0400

    Epoll Ctl ops and event type definitions added. Refactor epoll mod into main crate file.

--- src/lib.rs
+++ src/lib.rs
@@ -8,18 +8,72 @@
 extern crate libc;
 extern crate errno;
 
-use util::{CreateError, CtrlError, WaitError};
+use std::os::unix::io::RawFd;
+
+use self::errno::errno;
+use self::libc::consts::os::posix88;
+use self::libc::{size_t, c_void, c_int, ssize_t};
+
+use util::{CreateError, CtrlError, WaitError, CtlOp};
 
 
-mod epoll;
 mod util;
 
 
 /// Represents the result of calling epoll_create1
-pub type CreateResult = Result<i32, CreateError>;
+pub type CreateResult = Result<RawFd, CreateError>;
 
 /// Represents the result of calling epoll_ctrl
-pub type CtrlResult = Result<i32, CtrlError>;
+pub type CtlResult = Result<RawFd, CtlError>;
 
 /// Represents the result of calling epoll_wait
-pub type WaitResult = Result<i32, WaitError>;
+pub type WaitResult = Result<RawFd, WaitError>;
+
+
+#[cfg(target_arch = "x86_64")]
+#[repr(C, packed)]
+pub struct EpollEvent {
+    events: u32,
+    data: u64
+}
+
+#[cfg(not(target_arch = "x86_64"))]
+#[repr(C)]
+pub struct EpollEvent {
+    events: u32,
+    data: u64
+}
+
+extern "C" {
+    fn epoll_create1(flags: c_int) -> c_int;
+    fn epoll_ctl(epfd: c_int, op: c_int, fd: c_int,
+        event: *const EpollEvent) -> c_int;
+    fn epoll_wait(epfd: c_int, event: *mut EpollEvent,
+        maxevents: c_int, timeout: c_int) -> c_int;
+}
+
+/// Attempts to create a new epoll instance
+pub fn create1(flags: u32) -> CreateResult {
+    let mut epoll_fd;
+    unsafe {
+        epoll_fd = epoll::create1(flags as c_int);
+    }
+
+    if epoll_fd == -1 {
+        let errno = errno().0 as i32;
+        return match errno {
+            posix88::EINVAL     => Err(SetFdError::EINVAL),
+            posix88::EMFILE     => Err(SetFdError::EMFILE),
+            posix88::ENFILE     => Err(SetFdError::ENFILE),
+            posix88::ENOMEM     => Err(SetFdError::ENOMEM),
+            _ => panic!("Unexpected errno: {}", errno)
+    }
+
+    Ok(epoll_fd)
+}
+
+/// asdf
+pub fn ctl(epoll_fd: RawFd, op: CtlOp,
+           socket_fd: RawFd, event: Box<EpollEvent>) -> CtlResult {
+
+}
--- src/util.rs
+++ src/util.rs
@@ -6,7 +6,54 @@
 
 
 use std::fmt;
+use std::num::FromPrimitive;
 
+use super::libc::consts::os::posix88;
+
+
+#[derive(Clone)]
+pub enum CtlOp {
+    /// Register the target file descriptor fd on the epoll instance
+    /// referred to by the file descriptor epfd and associate the
+    /// event with the internal file linked to fd.
+    ADD = 1,
+    /// Change the event event associated with the target file descriptor fd.
+    MOD = 2,
+    /// Remove (deregister) the target file descriptor fd from the epoll
+    /// instance referred to by epfd. The event is ignored and can be NULL
+    DEL = 3
+}
+
+#[derive(FromPrimitive)]
+pub enum EventType {
+    /// The associated file is available for read(2) operations.
+    EPOLLIN = 0x001,
+    /// The associated file is available for write(2) operations.
+    EPOLLOUT = 0x004,
+    /// Stream socket peer closed connection, or shut down writing
+    /// half of connection.
+    EPOLLRDHUP = 0x2000,
+    /// There is urgent data available for read(2) operations.
+    EPOLLPRI = 0x002,
+    /// Error condition happened on the associated file descriptor.
+    /// epoll_wait(2) will always wait for this event; it is not
+    /// necessary to set it in events.
+    EPOLLERR = 0x008,
+    /// Hang up happened on the associated file descriptor. epoll_wait(2)
+    /// will always wait for this event; it is not necessary to set it
+    /// in events.
+    EPOLLHUP = 0x010,
+    /// Sets the Edge Triggered behavior for the associated file descriptor.
+    /// The default behavior for epoll is Level Triggered.
+    EPOLLET = (1 << 31),
+    /// Sets the one-shot behavior for the associated file descriptor.
+    /// This means that after an event is pulled out with epoll_wait(2)
+    /// the associated file descriptor is internally disabled and no other
+    /// events will be reported by the epoll interface. The user must call
+    /// epoll_ctl() with EPOLL_CTL_MOD to rearm the file descriptor with a
+    /// new event mask.
+    EPOLLONESHOT = (1 << 30)
+}
 
 #[derive(Debug, Clone)]
 pub enum CreateError {
@@ -22,8 +69,8 @@ pub enum CreateError {
 }
 
 #[derive(Debug, Clone)]
-pub enum CtrlError {
-    ///  fd is not a valid file descriptor.
+pub enum CtlError {
+    /// fd is not a valid file descriptor.
     EBADF,
     /// op was EPOLL_CTL_ADD, and the supplied file descriptor fd is already
     /// registered with this epoll instance.
@@ -70,15 +117,15 @@ impl fmt::Display for CreateError {
     }
 }
 
-impl fmt::Display for CtrlError {
+impl fmt::Display for CtlError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
-            CtrlError::EBADF => "EBADF".fmt(f),
-            CtrlError::EEXIST => "EEXIST".fmt(f),
-            CtrlError::EINVAL => "EINVAL".fmt(f),
-            CtrlError::ENOENT => "ENOENT".fmt(f),
-            CtrlError::ENOSPC => "ENOSPC".fmt(f),
-            CtrlError::EPERM => "EPERM".fmt(f)
+            CtlError::EBADF => "EBADF".fmt(f),
+            CtlError::EEXIST => "EEXIST".fmt(f),
+            CtlError::EINVAL => "EINVAL".fmt(f),
+            CtlError::ENOENT => "ENOENT".fmt(f),
+            CtlError::ENOSPC => "ENOSPC".fmt(f),
+            CtlError::EPERM => "EPERM".fmt(f)
         }
     }
 }
